e95a13e3382ed9f891c40531667e758a6c4a93a6
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-09 21:51:05 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
+require 'pry'
+def countdown (interger)
+  number = interger
+#  binding.pry
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep (interger)
+  number = interger
+#  binding.pry
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
 end
