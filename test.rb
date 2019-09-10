ed77186efc14841aa50b23bb9e1c4300fbb022d7
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-10 21:40:20 +0000

    Done.

---
+++ test.rb
@@ -0,0 +1,12 @@
+
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1 
+  end 
+  puts "HAPPY NEW YEAR!"
+end
+
+countdown_with_sleep(3)
